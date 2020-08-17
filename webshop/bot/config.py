7206942d17f20edb135532edb8026f2cf48d9a5e
sergiy <kcepriu@gmail.com>
2020-08-17 20:42:20 +0300

    17-08-2020

--- webshop/bot/config.py
+++ webshop/bot/config.py
@@ -1,6 +1,6 @@
 TOKEN = '1212570932:AAHdVivhzJGMjZbWWqvNwkfXdSN1Ad8Ch8w'
 NO_PHOTO_URL = 'https://forward-ua.com/upload/redsign_devfunc_nophoto/79b/no_photo.png'
 
-GLOBAL_IP = '34.89.129.211
+GLOBAL_IP = '34.89.129.211'
 WEBHOOK_PRIFIX = '/tg'
 WEBHOOK_URL = f'https://{GLOBAL_IP}{WEBHOOK_PRIFIX}'
