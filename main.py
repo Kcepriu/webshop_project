a567f77eac658dea6daf733dc7eddd2b019ddcca
sergiy <kcepriu@gmail.com>
2020-08-17 19:24:33 +0300

    17-08-2020

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 from webshop.bot import bot_instance
+from flask import Flask, abort
+from .webshop.bot.config import WEBHOOK_PRIFIX, WEBHOOK_URL
+from flask import request
+from telebot.types import Update
+
+app = Flask(__name__)
+@app.route(WEBHOOK_PRIFIX, metods=['POST'])
+def webhook():
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data().decode('utf-8')
+        update = Update.de_json(json_string)
+        bot_instance.process_new_shipping_query([update])
+
+
+        return
+    else:
+        abort(403)
+
+
+
+
 
 if __name__ == '__main__':
-    bot_instance.polling()
+    # bot_instance.polling()
     # bot_instance.infinity_polling()
+    bot_instance.set_webhook(
+        url=WEBHOOK_URL,
+        certificate=open('webhook_cer.pem', 'r')
+    )
--- webshop/bot/config.py
+++ webshop/bot/config.py
@@ -1,2 +1,6 @@
 TOKEN = '1212570932:AAHdVivhzJGMjZbWWqvNwkfXdSN1Ad8Ch8w'
 NO_PHOTO_URL = 'https://forward-ua.com/upload/redsign_devfunc_nophoto/79b/no_photo.png'
+
+GLOBAL_IP = '8.8.8.8'
+WEBHOOK_PRIFIX = '/tg'
+WEBHOOK_URL = f'https://{GLOBAL_IP}{WEBHOOK_PRIFIX}'
