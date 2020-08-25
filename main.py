d7b2b574cc86e1a5c4d4972b4d8a0a878920bab9
Kcepriu <kcepriu@gmail.com>
2020-08-25 15:08:42 +0300

    delete

--- main.py
+++ main.py
@@ -13,23 +13,20 @@ def webhook():
         json_string = request.get_data().decode('utf-8')
         update = Update.de_json(json_string)
         bot_instance.process_new_updates([update])
-
-
         return ''
     else:
         abort(403)
 
-
-
-
-
 if __name__ == '__main__':
-    # bot_instance.polling()
-    # bot_instance.infinity_polling()
     bot_instance.remove_webhook()
     time.sleep(1)
-    bot_instance.set_webhook(
-        url=WEBHOOK_URL,
-        certificate=open('webhook_cert.pem', 'r')
-    )
-    app.run(debug=True)
+    bot_instance.polling()
+    # bot_instance.infinity_polling()
+
+    # bot_instance.remove_webhook()
+    # time.sleep(1)
+    # bot_instance.set_webhook(
+    #     url=WEBHOOK_URL,
+    #     certificate=open('webhook_cert.pem', 'r')
+    # )
+    # app.run(debug=True)
